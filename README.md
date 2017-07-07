ac2221cb9116c0b916faaf1cd2f356b97a12d9e1
Fabio Di Peri <fabiodp@cityglance.it>
2017-07-07 12:28:16 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Xamarin.UIDebuggingInformationOverlay
+
+A simple binding for Apple's internal UIDebuggingInformationOverlay, ported from Ryan Peterson's [blog post](http://ryanipete.com/blog/ios/swift/objective-c/uidebugginginformationoverlay/).
+
+Blog post about the porting [here](https://kipters.net/post/ios-debugging-overlay/).
+
+## How to use
+
+### Source
+
+Just copy `UIDebuggingInformationOverlay.cs` in your project, it works as-is.
+
+### NuGet
+
+`Install-Package Kipware.UIDebuggingInformationOverlay`
+
+## Disclaimer
+
+This package calls iOS internal APIs, using this may cause your app to be rejected from the App Store. Use at your own risk.
---
+++ UIDebuggingInformationOverlay/UIDebuggingInformationOverlay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ObjCRuntime;
+
+namespace Kipters.Helpers.iOS
+{
+    public class UIDebuggingInformationOverlay
+    {
+        [DllImport(Constants.ObjectiveCLibrary, EntryPoint = "objc_msgSend")]
+        private static extern IntPtr objc_msgSend(IntPtr target, IntPtr selector);
+
+        private static readonly IntPtr OverlayClass = Class.GetHandle("UIDebuggingInformationOverlay");
+        private static readonly Selector PrepareSelector = new Selector("prepareDebuggingOverlay");
+        private static readonly Selector OverlaySelector = new Selector("overlay");
+        private static readonly Selector ToggleSelector = new Selector("toggleVisibility");
+
+        private readonly IntPtr _overlay;
+
+        public UIDebuggingInformationOverlay() => _overlay = objc_msgSend(OverlayClass, OverlaySelector.Handle);
+        public static void PrepareDebuggingOverlay() => objc_msgSend(OverlayClass, PrepareSelector.Handle);
+        public void ToggleVisibility() => objc_msgSend(_overlay, ToggleSelector.Handle);
+    }
+}
